f24db358fee1f26d6f5e5d629e106d4b419a1351
Alex Malz <aimalz@nyu.edu>
2022-06-29 10:51:54 -0700

    syncing final version of code actually run

--- scripts/eval_photo-zs_SNIbc_Pt1.sh
+++ scripts/eval_photo-zs_SNIbc_Pt1.sh
@@ -3,15 +3,15 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
 #SBATCH --mem=1951
-#SBATCH --time=06:45:00
+#SBATCH --time=05:30:00
 #SBATCH --constraint=haswell
 #SBATCH --qos=shared
 #SBATCH --output=%x.%j.out
-#SBATCH --array=26-63
+#SBATCH --array=0-63
 #SBATCH --account=m1727
 
 
 source /global/u2/a/aimalz/ve3_elasticc/bin/activate
 export HDF5_USE_FILE_LOCKING=FALSE
 export DESC_GCR_SITE='nersc'
-srun /usr/bin/time /global/u2/a/aimalz/ve3_elasticc/bin/python eval_photo-zs.py SNIa
+srun /usr/bin/time /global/u2/a/aimalz/ve3_elasticc/bin/python eval_photo-zs.py SNIbc_Pt1
